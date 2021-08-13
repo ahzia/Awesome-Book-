6386c6428a5d01846d24160df8d261c35707846c
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-08-13 13:45:33 +0430

    solve linters issue

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 import Books from './books.js';
 
 function assignDate() {
-  const DateTime = luxon.DateTime.now();
-  let Date = DateTime.year + "-" + DateTime.month + "-"+DateTime.day;
-  document.getElementById("date").innerHTML = Date;
+  const DateTime = luxon.DateTime.now(); // eslint-disable-line no-undef
+  const Date = `${DateTime.year} - ${DateTime.month} - ${DateTime.day}`;
+  document.getElementById('date').innerHTML = Date;
 }
 
 const newBooks = new Books();
@@ -17,32 +17,30 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-function Tabs() {
-  var bindAll = function() {
-    var menuElements = document.querySelectorAll('[data-tab]');
-    for(var i = 0; i < menuElements.length ; i++) {
-      menuElements[i].addEventListener('click', change, false);
-    }
+const clear = () => {
+  const menuElements = document.querySelectorAll('[data-tab]');
+  var i;
+  for (i = 0; i < menuElements.length; i+=1) {
+    menuElements[i].classList.remove('active-tab');
+    var id = menuElements[i].getAttribute('data-tab');
+    document.getElementById(id).classList.remove('active-tab');
   }
-
-  var clear = function() {
-    var menuElements = document.querySelectorAll('[data-tab]');
-    for(var i = 0; i < menuElements.length ; i++) {
-      menuElements[i].classList.remove('active-tab');
-      var id = menuElements[i].getAttribute('data-tab');
-      document.getElementById(id).classList.remove('active-tab');
-    }
-  }
-
-  var change = function(e) {
-    clear();
-    e.target.classList.add('active-tab');
-    var id = e.currentTarget.getAttribute('data-tab');
-    document.getElementById(id).classList.add('active-tab');
-  }
-
-  bindAll();
 }
 
-var connectTabs = new Tabs();
+const change = (e) => {
+  clear();
+  e.target.classList.add('active-tab');
+  const id = e.currentTarget.getAttribute('data-tab');
+  document.getElementById(id).classList.add('active-tab');
+}
+
+const bindAll= () => {
+  const menuElements = document.querySelectorAll('[data-tab]');
+  var i;
+  for (i = 0; i < menuElements.length; i+=1) {
+    menuElements[i].addEventListener('click', change, false);
+  }
+}
+
+bindAll();
 assignDate();
