cec291e9b1e79229c867840a4031c9a25f73c48b
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-08-13 13:53:11 +0430

    fix linters isuue

--- js/index.js
+++ js/index.js
@@ -19,28 +19,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const clear = () => {
   const menuElements = document.querySelectorAll('[data-tab]');
-  var i;
-  for (i = 0; i < menuElements.length; i+=1) {
+  let i;
+  for (i = 0; i < menuElements.length; i += 1) {
     menuElements[i].classList.remove('active-tab');
-    var id = menuElements[i].getAttribute('data-tab');
+    const id = menuElements[i].getAttribute('data-tab');
     document.getElementById(id).classList.remove('active-tab');
   }
-}
+};
 
 const change = (e) => {
   clear();
   e.target.classList.add('active-tab');
   const id = e.currentTarget.getAttribute('data-tab');
   document.getElementById(id).classList.add('active-tab');
-}
+};
 
-const bindAll= () => {
+const bindAll = () => {
   const menuElements = document.querySelectorAll('[data-tab]');
-  var i;
-  for (i = 0; i < menuElements.length; i+=1) {
+  let i;
+  for (i = 0; i < menuElements.length; i += 1) {
     menuElements[i].addEventListener('click', change, false);
   }
-}
+};
 
 bindAll();
 assignDate();
